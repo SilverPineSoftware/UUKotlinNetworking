9041c59b43c5a89e376636ef988156a175343abd
Ryan DeVore <ryan@silverpine.com>
2025-10-12 10:14:56 -0700

    Stub out a shutter stock screen

--- app/src/main/java/com/silverpine/uu/sample/networking/MainActivity.kt
+++ app/src/main/java/com/silverpine/uu/sample/networking/MainActivity.kt
@@ -5,11 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ChatBubble
+import androidx.compose.material.icons.filled.Image
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.DrawerValue
@@ -17,7 +17,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
@@ -26,20 +25,18 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import com.silverpine.uu.sample.networking.openai.OpenAiScreen
-import com.silverpine.uu.sample.networking.openai.OpenAiSettingsScreen
 import com.silverpine.uu.sample.networking.openai.SettingsRepository
+import com.silverpine.uu.sample.networking.shutterstock.ShutterStockScreen
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity()
@@ -58,9 +55,8 @@ class MainActivity : ComponentActivity()
 
 sealed class AppScreen(val label: String, val icon: ImageVector)
 {
-    object Home : AppScreen("Home", Icons.Default.Home)
-    object Settings : AppScreen("Settings", Icons.Default.Settings)
-    object OpenAi : AppScreen("Open AI", Icons.Default.Home)
+    object OpenAi : AppScreen("Open AI", Icons.Default.ChatBubble)
+    object ShutterStock : AppScreen("Shutterstock", Icons.Default.Image)
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -72,15 +68,12 @@ fun MainScreen()
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
 
-    //var showSettings by remember { mutableStateOf(false) }
-    //val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    var currentScreen by remember { mutableStateOf<AppScreen>(AppScreen.OpenAi) }
 
-    var currentScreen by remember { mutableStateOf<AppScreen>(AppScreen.Home) }
+    val drawerItems = listOf(AppScreen.OpenAi, AppScreen.ShutterStock)
 
-    val drawerItems = listOf(AppScreen.Home, AppScreen.Settings, AppScreen.OpenAi)
-
-    // State that controls whether the OpenAI settings sheet should be visible
     var showOpenAiSettings by remember { mutableStateOf(false) }
+    var showShutterstockSettings by remember { mutableStateOf(false) }
 
     ModalNavigationDrawer(
         drawerState = drawerState,
@@ -114,22 +107,20 @@ fun MainScreen()
                     actions = {
                         when (currentScreen)
                         {
-                            is AppScreen.Home -> {
-                                /*IconButton(onClick = { /* do something */ }) {
-                                    Icon(Icons.Default.Settings, contentDescription = "Home action")
-                                }*/
-                            }
-                            is AppScreen.Settings -> {
-                                /*IconButton(onClick = { /* do something */ }) {
-                                    Icon(Icons.Default.Home, contentDescription = "Settings action")
-                                }*/
-                            }
                             is AppScreen.OpenAi -> {
                                 IconButton(onClick = {
                                     showOpenAiSettings = true
                                 })
                                 {
                                     Icon(Icons.Default.Settings, contentDescription = "Open AI Settings")
+                                }
+                            }
+                            is AppScreen.ShutterStock -> {
+                                IconButton(onClick = {
+                                    showShutterstockSettings = true
+                                })
+                                {
+                                    Icon(Icons.Default.Settings, contentDescription = "Shutterstock Settings")
                                 }
                             }
                         }
@@ -141,51 +132,18 @@ fun MainScreen()
             {
                 when (currentScreen)
                 {
-                    is AppScreen.Home -> HomeScreen()
-                    is AppScreen.Settings -> SettingsScreen()
                     is AppScreen.OpenAi -> OpenAiScreen(
                         repository = repository,
                         showSettings = showOpenAiSettings,
                         onSettingsDismissed = { showOpenAiSettings = false }
                     )
-                }
-            }
-
-            /*if (showSettings)
-            {
-                ModalBottomSheet(
-                    onDismissRequest = { showSettings = false },
-                    sheetState = sheetState
-                ) {
-                    OpenAiSettingsScreen(
-                        repository = repository,
-                        onClose = {
-                            scope.launch { sheetState.hide() }
-                                .invokeOnCompletion { showSettings = false }
-                        }
+                    is AppScreen.ShutterStock -> ShutterStockScreen(
+                        //repository = repository,
+                        showSettings = showShutterstockSettings,
+                        onSettingsDismissed = { showShutterstockSettings = false }
                     )
                 }
-            }*/
+            }
         }
-    }
-}
-
-@Composable
-fun HomeScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text("This is the Home Screen")
-    }
-}
-
-@Composable
-fun SettingsScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text("This is the Settings Screen")
     }
 }
---
+++ app/src/main/java/com/silverpine/uu/sample/networking/shutterstock/ShutterStockScreen.kt
@@ -0,0 +1,23 @@
+package com.silverpine.uu.sample.networking.shutterstock
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ShutterStockScreen(
+    showSettings: Boolean,
+    onSettingsDismissed: () -> Unit
+)
+{
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    )
+    {
+        Text("Coming soon!")
+    }
+}
