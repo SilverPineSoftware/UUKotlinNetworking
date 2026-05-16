a5002c55cac7548c4df9a193727a0c68b78bca0a
Ryan DeVore <ryan@silverpine.com>
2026-05-15 21:07:51 -0700

    Refactor vars

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,3 @@
-extra["uu_namespace"] = "com.silverpine.uu.networking"
-extra["uu_publish_artifact_id"] = "uu-networking-ktx"
-extra["uu_publish_description"] = "Useful Utilities Networking"
-extra["uu_scm_module_name"] = "UUKotlinNetworking"
-
-extra["uu_min_sdk"] = 26
-extra["uu_target_sdk"] = 36
-extra["uu_java_version"] = 17
-
 plugins {
     alias(uuBuild.plugins.android.application) apply false
     alias(uuBuild.plugins.android.library) apply false
